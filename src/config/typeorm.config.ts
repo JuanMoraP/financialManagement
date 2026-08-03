77a2e3c3ec3154a4e8ebcfb978f548e82872925b
JuanMoraP <juanesmo888@gmail.com>
2026-08-02 21:21:49 -0600

    Agregamos una configuración en app.module.ts y en typeorm.config para separar BD local y BD produccion. Adicionalmente ahora hay .env.development y .env.production

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -2,14 +2,15 @@ import { DataSource } from 'typeorm';
 import { config } from 'dotenv';
 
 // Carga el .env manualmente porque NestJS no está corriendo aquí
-config();
+config({ path: `.env.${process.env.NODE_ENV || 'development'}` });
 
 export default new DataSource({
   type: 'postgres',
   url: process.env.DATABASE_URL,
   entities: ['src/**/*.entity{.ts,.js}'],
   migrations: ['src/migrations/*.ts'],
-  ssl: {
-    rejectUnauthorized: false,
-  },
+  ssl:
+    process.env.NODE_ENV === 'production'
+      ? { rejectUnauthorized: false }
+      : false,
 });

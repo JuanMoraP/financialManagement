0346a7792d1cdece4327348ce97aae896cf8b1d7
JuanMoraP <juanesmo888@gmail.com>
2026-08-03 21:11:19 -0600

    Se realizó el signUp incluyendo el dto, sin embargo hace falta corregirle unas cosas que están anotadas en el documento general

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,10 +3,12 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { RefreshToken } from './entities/refresh-token.entity';
+import { UsersRepository } from '../users/users.repository';
+import { User } from '../users/entities/users.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([RefreshToken])],
+  imports: [TypeOrmModule.forFeature([RefreshToken, User])],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, UsersRepository],
 })
 export class AuthModule {}
--- src/users/users.repository.ts
+++ src/users/users.repository.ts
@@ -1,7 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './entities/users.entity';
 import { Repository } from 'typeorm';
+import { SignUpDto } from '../auth/dto/signup.dto';
 
 @Injectable()
 export class UsersRepository {
@@ -22,5 +23,18 @@ export class UsersRepository {
   getUserByEmail(email: string) {
     const user = this.usersRepository.findOne({ where: { email } });
     return user;
+  }
+
+  async signUp(newUser: SignUpDto) {
+    const email = (newUser.email || '').trim().toLocaleLowerCase();
+    const emailExist = await this.getUserByEmail(email);
+    if (emailExist) {
+      throw new BadRequestException('El email ya está registrado');
+    }
+
+    const user = this.usersRepository.create({ ...newUser, email });
+    await this.usersRepository.save(user);
+
+    return 'El usuario ha sido creado correctamente';
   }
 }
